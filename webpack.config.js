6eff3b51723f372ce0deec2a9944c0273ae14fbb
marek <kursygitarowekopernik@gmail.com>
2018-02-22 18:13:33 +0100

    env

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,28 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var OptimizeJsPlugin = require('optimize-js-plugin');
+var env = process.env.NODE_ENV || 'development';
+var plugins = [
+    new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        })
+    ];
+
+    console.log('NODE_ENV:', env);
+
+    if (env === 'production') {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeJsPlugin({
+          sourceMap: false
+        })
+      );
+    }
+
+console.log('NODE_ENV:', env);
+
 
 module.exports = {
     entry: './src/index.js',
@@ -30,13 +52,5 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        }), 
-        new webpack.optimize.UglifyJsPlugin(),
-        new OptimizeJsPlugin({
-            sourceMap: false})]
-};
+    plugins: plugins,
+}
