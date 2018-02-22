9b533d7c0ec1cd417e7d4aa466e4a943bd67a8fb
marek <kursygitarowekopernik@gmail.com>
2018-02-22 17:38:59 +0100

    .

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
         const data = [...this.state.data, todo];
         this.setState({data});
     }
-    removeTodo(id) {
+    removeTodo = (id) => {
         const remainder = this.state.data.filter(todo => todo.id !== id);
         this.setState({data: remainder});
     }
